77d31ca6ad7f8555784426c86ac7f70c7a26c39a
fwam <contact@fwam.fyi>
2026-03-17 17:19:18 +0100

    Added macro expander.
    Stolen from (or rather, forcibly provided by) @vifon.

--- lisp/20-functions.el
+++ lisp/20-functions.el
@@ -3,3 +3,17 @@
   (interactive)
   (let ((vertico-sort-function #'vertico-sort-alpha))
     (call-interactively #'find-file)))
+
+(define-advice pp-macroexpand-expression
+    (:around (orig expression) use-package-minimal)
+  "Enable `use-package-expand-minimally' when expanding `use-package' forms.
+
+This greatly reduces the noise created by the error handling that
+`use-package' does otherwise.  Do not use this advice when specifically
+debugging this error handling!"
+  (if (eq (car expression) 'use-package)
+      (let ((use-package-expand-minimally t))
+        (funcall orig expression))
+    (funcall orig expression)))
+
+(bind-key "C-x M-e" #'pp-macroexpand-last-sexp)
