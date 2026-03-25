24a2d5ac9220dc1457d836557ca74d8fe869b18b
Xenia <contact@fwam.fyi>
2026-03-25 18:32:31 +0100

    Fixed evil after accidentally breaking it during rebase.

--- lisp/30-evil.el
+++ lisp/30-evil.el
@@ -1,12 +1,9 @@
 (setq evil-want-keybinding nil)
 (use-package evil
-<<<<<<< HEAD
-  :hook (after-init . evil-mode))
-=======
   :config
   (setq evil-transient-mouse-selection t)
-  :init ( evil-mode))
->>>>>>> c58a2e2 (Switched from straight to elpaca)
+  :init (evil-mode))
+
 (use-package evil-collection
   :straight (evil-collection :type git
 			     :host github
