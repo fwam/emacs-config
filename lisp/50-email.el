f482430710219df3bd5c5237abd096ff465aa6a1
Xenia <contact@fwam.fyi>
2026-03-25 18:37:35 +0100

    Add notmuch.

--- lisp/50-email.el
+++ lisp/50-email.el
@@ -1,1 +1,10 @@
-(use-package notmuch)
+(use-package notmuch
+  :config
+  (if (eq system-type 'darwin)
+      (setq sendmail-program "/opt/homebrew/bin/msmtp")
+      (setq sendmail-program "/usr/bin/msmtp"))
+
+  (setq send-mail-function 'sendmail-send-it
+      mail-specify-envelope-from t
+      message-sendmail-envelope-from 'header
+      mail-envelope-from 'header))
